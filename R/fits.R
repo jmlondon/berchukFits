31442e422b07e8f9045ecc67c7b87d5ca5c4b984
Josh M. London <josh.london@noaa.gov>
2023-10-31 11:41:50 -0700

    update data from latest fit results

--- R/fits.R
+++ R/fits.R
@@ -28,37 +28,6 @@
 #' }
 "ribbon_fit"
 
-#' Fitted glmmLDTS Model Object for Ribbon Seals - No Age/Sex
-#'
-#' A fitted glmmLDTS model object for ribbon seals without inclusion of
-#' age or sex covariates in the model formula
-#'
-#' @format A glmmLDTS object with 21 elements:
-#' \describe{
-#'   \item{dataset}{data used for model fit}
-#'   \item{WARNINGS}{}
-#'   \item{fixed.formula}{class `formula` for fixed effects}
-#'   \item{random.formula}{class `formula` for random effects}
-#'   \item{sample.size}{number of records in the data set}
-#'   \item{timecol}{column that specifies the time vector}
-#'   \item{trialscol}{}
-#'   \item{group.vec}{column identifying groups}
-#'   \item{ridge.reg}{}
-#'   \item{lambda}{}
-#'   \item{start.time}{start time for model fit}
-#'   \item{end.time}{end time for model fit}
-#'   \item{R.cov.paramaters}{}
-#'   \item{G.cov.parameters}{}
-#'   \item{fixed.effects}{fixed effects from the model}
-#'   \item{random.effects}{random effects from the model}
-#'   \item{typeIII.hypoth}{}
-#'   \item{fit.table}{}
-#'   \item{covb}{}
-#'   \item{outer.iterations}{}
-#'   \item{inner.iterations2}{}
-#' }
-"ribbon_noagesex_fit"
-
 #' Fitted glmmLDTS Model Object for Ribbon Seals - With Year
 #'
 #' A fitted glmmLDTS model object for ribbon seals that includes
@@ -119,37 +88,6 @@
 #'   \item{inner.iterations2}{}
 #' }
 "spotted_fit"
-
-#' Fitted glmmLDTS Model Object for Spotted Seals - No Age/Sex
-#'
-#' A fitted glmmLDTS model object for spotted seals without inclusion of
-#' age or sex covariates in the model formula
-#'
-#' @format A glmmLDTS object with 21 elements:
-#' \describe{
-#'   \item{dataset}{data used for model fit}
-#'   \item{WARNINGS}{}
-#'   \item{fixed.formula}{class `formula` for fixed effects}
-#'   \item{random.formula}{class `formula` for random effects}
-#'   \item{sample.size}{number of records in the data set}
-#'   \item{timecol}{column that specifies the time vector}
-#'   \item{trialscol}{}
-#'   \item{group.vec}{column identifying groups}
-#'   \item{ridge.reg}{}
-#'   \item{lambda}{}
-#'   \item{start.time}{start time for model fit}
-#'   \item{end.time}{end time for model fit}
-#'   \item{R.cov.paramaters}{}
-#'   \item{G.cov.parameters}{}
-#'   \item{fixed.effects}{fixed effects from the model}
-#'   \item{random.effects}{random effects from the model}
-#'   \item{typeIII.hypoth}{}
-#'   \item{fit.table}{}
-#'   \item{covb}{}
-#'   \item{outer.iterations}{}
-#'   \item{inner.iterations2}{}
-#' }
-"spotted_noagesex_fit"
 
 #' Fitted glmmLDTS Model Object for Spotted Seals - With Year
 #'
